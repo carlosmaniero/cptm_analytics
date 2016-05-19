920ed8bcdb33ec25e6bc6940b77dba22796fde86
Carlos Maniero <carlosmaniero@gmail.com>
2016-05-19 00:52:07 -0300

    Add line test

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -8,3 +8,14 @@ def test_download_data():
     response = yield crawler.download_data()
 
     assert response.status_code == 200
+
+
+@pytest.mark.gen_test
+def test_parse_content():
+    crawler = Crawler()
+    response = yield crawler.download_data()
+
+    parsed = yield crawler.parse_content(response.content)
+
+    for line in Crawler.LINES:
+        assert line in parsed
